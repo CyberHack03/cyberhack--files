400b1280558e514bb4bf32bd2993b21839153ca3
CyberHack03 <117557363+CyberHack03@users.noreply.github.com>
2022-11-28 11:10:10 -0500

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -16,28 +16,28 @@ scan = input("""\nplease enter the type of scan you want to run
                 3)Comprehensive scan \n """)
 
 if scan == '1':
-    print("nmap version: ",scanner.nmap_version())
-    scanner.scan(ip_addr,'1-1000','-v -sS')
-    print(scanner.scaninfo())
-    print("IP status: ",scanner[ip_addr].state())
-    print(scanner[ip_addr].allprotocols())
-    print("open ports: ",scanner[ip_addr]['tcp'].keys())
+    print("nmap version: ",Scanner.nmap_version())
+    Scanner.scan(ip_addr,'1-1000','-v -sS')
+    print(Scanner.scaninfo())
+    print("IP status: ",Scanner[ip_addr].state())
+    print(Scanner[ip_addr].allprotocols())
+    print("open ports: ",Scanner[ip_addr]['tcp'].keys())
 
 elif scan == '2':
-    print("nmap version: ",scanner.nmap_version())
-    scanner.scan(ip_addr,'1-1000','-v -sU')
-    print(scanner.scaninfo())
-    print("IP status: ",scanner[ip_addr].state())
-    print(scanner[ip_addr].allprotocols())
-    print("open ports: ",scanner[ip_addr]['udp'].keys())
+    print("nmap version: ",Scanner.nmap_version())
+    Scanner.scan(ip_addr,'1-1000','-v -sU')
+    print(Scanner.scaninfo())
+    print("IP status: ",Scanner[ip_addr].state())
+    print(Scanner[ip_addr].allprotocols())
+    print("open ports: ",Scanner[ip_addr]['udp'].keys())
 
 elif scan == '3':
-    print("nmap version: ",scanner.nmap_version())
-    scanner.scan(ip_addr,'1-1000','-v -sS -sV -sC -A -O')
-    print(scanner.scaninfo())
-    print("IP status: ",scanner[ip_addr].state())
-    print(scanner[ip_addr].allprotocols())
-    print("open ports: ",scanner[ip_addr]['tcp'].keys())
+    print("nmap version: ",Scanner.nmap_version())
+    Scanner.scan(ip_addr,'1-1000','-v -sS -sV -sC -A -O')
+    print(Scanner.scaninfo())
+    print("IP status: ",Scanner[ip_addr].state())
+    print(Scanner[ip_addr].allprotocols())
+    print("open ports: ",Scanner[ip_addr]['tcp'].keys())
 
 else:
     print("please enter a valid option")
