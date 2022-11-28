3d98cdc03508fd24d7b68f4d33184678e1fd1694
CyberHack03 <117557363+CyberHack03@users.noreply.github.com>
2022-11-28 11:12:18 -0500

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -16,7 +16,7 @@ scan = input("""\nplease enter the type of scan you want to run
                 3)Comprehensive scan \n """)
 
 if scan == '1':
-    print("nmap version: ",Scanner.nmap_version())
+    print("nmap version: ",PortScanner.nmap_version())
     Scanner.scan(ip_addr,'1-1000','-v -sS')
     print(Scanner.scaninfo())
     print("IP status: ",Scanner[ip_addr].state())
